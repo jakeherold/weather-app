65da68877a11f1bafd4ffe3d795822b51b801aa3
jakeherold <jherold.developer@gmail.com>
2019-06-02 09:47:27 -0700

    got table cleaning queries sorted, to prevent undue table growth

--- weather_server.py
+++ weather_server.py
@@ -8,8 +8,9 @@ c = conn.cursor()
 
 # Faux data for entry
 # ts is timestamp in epoch
+# We're declaring it as 
 ts = int(time.time())
-temp_k = 254.4
+temp_k = 256.4
 
 # Create Table
 c.execute("""CREATE TABLE if not exists weather(
@@ -19,16 +20,23 @@ c.execute("""CREATE TABLE if not exists weather(
 
 
 # Write data to table
-# c.execute("INSERT INTO weather VALUES (:epoch_time, :temp_k)",{'epoch_time': ts, 'temp_k':temp_k,} )
-# conn.commit()
+c.execute("INSERT INTO weather VALUES (:epoch_time, :temp_k)",{'epoch_time': ts, 'temp_k':temp_k,} )
+conn.commit()
 
 # Read Data from table
-# c.execute("SELECT * FROM weather")
-# print(c.fetchall())
-
-# Trying to figure out how to tell if table exists. Trying in create method for now.
-c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{weather}';")
+c.execute("SELECT * FROM weather")
 print(c.fetchall())
 
+# Trying to figure out how to tell if table exists. Trying in create method for now.
+# c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{weather}';")
+# print(c.fetchall())
+
+# Delete Old rows (for updates to data, so we don't let the DB grow too big)
+
+c.execute("DELETE FROM weather WHERE epoch_time < ?", (ts,))
+conn.commit()
+
+c.execute("SELECT * FROM weather")
+print(c.fetchall())
 
 conn.close()
